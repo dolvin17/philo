7f1b9ba22fb50a69243a358e55e0c79362cc286d
Grace karol Huertas collaguazo <ghuertas@c1r8s1.42madrid.com>
2024-11-22 14:59:48 +0100

    feat:parsing args started

---
+++ main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 14:40:53 by dolvin17          #+#    #+#             */
+/*   Updated: 2024/11/22 14:46:02 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 6)
+		printf("argument amount is correct\n");
+	else
+		printf("argument amount is incorrect\n");
+}

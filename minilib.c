3231f998f1621df4a1fe0ee8373d7c63ba4148e1
dolvin17 <huertas876@gmail.com>
2024-11-23 19:28:07 +0100

    refactor: atoi doesnt need negative values as input

--- minilib.c
+++ minilib.c
@@ -6,7 +6,7 @@
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 17:56:09 by ghuertas          #+#    #+#             */
-/*   Updated: 2024/11/22 19:21:41 by dolvin17         ###   ########.fr       */
+/*   Updated: 2024/11/23 18:47:37 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,18 @@ size_t	ft_strlen(const char *str)
 long	ft_atol(const char *str)
 {
 	long	nbr;
-	long	sign;
 
 	nbr = 0;
-	sign = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '-')
-		checking(1, "Only positive values\n");
-	if (*str == '-' || *str == '+')
+	checking((*str == '-'), "Only positive values\n");
+	if (*str == '+')
 		str++;
 	while (*str >= '0' && *str <= '9')
 	{
 		nbr = nbr * 10 + (*str - '0');
-		checking(nbr > INT_MAX, "Error\n");
-		checking(nbr < INT_MIN, "Error\n");
+		checking(nbr > INT_MAX, "too long input\n");
 		str++;
 	}
-	return (nbr * sign);
+	return (nbr);
 }
